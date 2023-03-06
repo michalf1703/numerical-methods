7b4998c6cfad2ce8a16814b3a526aa1522b92c78
242383 Ferdzyn Michał <242383@edu.p.lodz.pl>
2023-03-06 23:21:14 +0100

    Zad1-zmiany_zmiany

--- bisekcja.py
+++ bisekcja.py
@@ -1,43 +1,43 @@
 import funkcje as f
 
 
-def bisekcjaEps(a, b, eps, flag):
-    fa = f.function(a,flag)
-    fb = f.function(b,flag)
-    iter = 0
+def bisekcjaEps(x_min, x_max, dokladnosc, wyborFunkcja):
+    fa = f.function(x_min,wyborFunkcja)
+    fb = f.function(x_max,wyborFunkcja)
+    iteracja = 0
     if fa * fb > 0:
         print("Funkcja nie spelnia zalozen (fa * fb >0) ")
     else:
         guardian = True
         while guardian is True:
-            iter += 1
-            x0 = (a + b) / 2
-            fx0 = f.function(x0, flag)
+            iteracja += 1
+            x0 = (x_min + x_max) / 2
+            fx0 = f.function(x0, wyborFunkcja)
 #warunek zakonczenia; WARIANT A
-            if abs(b - a) < eps:
-                return x0, iter
+            if abs(x_max - x_min) < dokladnosc:
+                return x0, iteracja
             if fx0 * fb < 0:
-                a = x0
+                x_min = x0
             else:
-                b = x0
+                x_max = x0
                 fb = fx0
 
 
-def bisekcjaIter(a, b, iter, flag):
-    fa = f.function(a,flag)
-    fb = f.function(b,flag)
+def bisekcjaIter(x_min, x_max, iteracja, wyborFunkcja):
+    fa = f.function(x_min,wyborFunkcja)
+    fb = f.function(x_max,wyborFunkcja)
     if fa * fb > 0:
         print("Funkcja nie spelnia zalozen (fa * fb >0) ")
     else:
-        while iter > 0:
-            x0 = (a + b) / 2
-            fx0 = f.function(x0, flag)
+        while iteracja > 0:
+            x0 = (x_min + x_max) / 2
+            fx0 = f.function(x0, wyborFunkcja)
             if fx0 == 0:
                 return x0
             if fx0 * fb < 0:
-                a = x0
+                x_min = x0
             else:
-                b = x0
+                x_max = x0
                 fb = fx0
-            iter = iter - 1
+            iteracja = iteracja - 1
         return x0
--- falsi.py
+++ falsi.py
@@ -1,29 +1,29 @@
 import funkcje as f
 
 
-def falsiEps(a, b, eps, flag):
-    fa = f.function(a,flag)
-    fb = f.function(b,flag)
-    iter = 0
+def falsiEps(x_min, x_max, dokladnosc, wyborFunkcja):
+    fa = f.function(x_min,wyborFunkcja)
+    fb = f.function(x_max,wyborFunkcja)
+    iteracja = 0
     if fa * fb > 0:
         print("Funkcja nie spełnia założeń (fa * fb >0) ")
     else:
         guardian = True
         while guardian is True:
-            iter += 1
-            x0 = b - (fb * (b - a)) / (fb - fa)
-            fx0 = f.function(x0, flag)
-            if abs(fx0) < eps:
-                return x0, iter
+            iteracja += 1
+            x0 = x_max - (fb * (x_max - x_min)) / (fb - fa)
+            fx0 = f.function(x0, wyborFunkcja)
+            if abs(fx0) < dokladnosc:
+                return x0, iteracja
             if fx0 * fb < 0:
-                a = x0
+                x_min = x0
                 fa = fx0
             else:
-                b = x0
+                x_max = x0
                 fb = fx0
 #warunek zakonczenia; WARIANT A
-            if abs(b - a) < eps:
-                return (a + b) / 2, iter
+            if abs(x_max - x_min) < dokladnosc:
+                return (x_min + x_max) / 2, iteracja
 
 def falsiIter(a, b, N, flag):
     fa = f.function(a,flag)
@@ -49,7 +49,6 @@ def falsiIter(a, b, N, flag):
                 print("Osiągnięto maksymalną liczbę iteracji.")
                 return x0
     return x0
-
 
 
 
