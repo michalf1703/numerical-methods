25d800c39291e9bdc118a4ff42a48184d2232ff1
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-04-20 23:01:00 +0200

    add functions.py file, choose_range() and choose_number_of_knots()

---
+++ zad3/functions.py
@@ -0,0 +1,25 @@
+import math
+
+def linear(x):
+    return 4 * x + 3
+
+
+def absolute_x(x):
+    return abs(x)
+
+
+def polynomial(x):
+    return ((2 * x + 2) * x + 4) * x - 1
+
+
+def trigonometric(x):
+    return math.sin(x)
+
+
+def submission_1(x):
+    return 4 * x * abs(x)
+
+
+def submission_2(x):
+    return math.sin(x) + ((2 * x + 2) * x + 4) * x - 1
+
--- zad3/main.py
+++ zad3/main.py
@@ -11,8 +11,21 @@ def draw_menu():
         print("6.   sin(x) + 2x^3 + 2x^2 + 4x - 1 \n")
         choice = int(input("Wybór funkcji : "))
 
+def choose_range():
+    print("Wybierz przedział: ")
+    lower_range = float(input("Dolny przedział: "))
+    upper_range = float(input("Górny przedział: "))
+    if lower_range > upper_range:
+        return upper_range, lower_range
+    return lower_range, upper_range
+
+def choose_number_of_knots():
+    return int(input("Podaj liczbę węzłów Czebyszewa: "))
+
 
 if __name__ == '__main__':
     draw_menu()
+    choose_range()
+
 
 
