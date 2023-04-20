2515b5e65689f171e7919c380783bf65162466ee
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-04-20 22:30:39 +0200

    add menu

--- zad3/main.py
+++ zad3/main.py
@@ -1,11 +1,18 @@
 
 
-def print_hi(name):
-
-    print("Zad3-numerki ")
+def draw_menu():
+    #while true:  --> to pozniej do petli
+        print("Wybierz rownanie: \n")
+        print("1.   4x + 3\n")
+        print("2.   |x|\n")
+        print("3.   2x^3 + 2x^2 + 4x - 1\n")
+        print("4.   sin(x)\n")
+        print("5.   4x * |x|\n")
+        print("6.   sin(x) + 2x^3 + 2x^2 + 4x - 1 \n")
+        choice = int(input("Wybór funkcji : "))
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    draw_menu()
 
 
